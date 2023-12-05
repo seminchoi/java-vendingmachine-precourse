933b901db69f9a5a7d7d2c1eb04c0cf2a25a9fdb
seminchoi <choicco89@gmail.com>
2023-12-05 16:26:58 +0900

    feat(view.InputView) : 메뉴를 입력받는 기능 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,14 +1,21 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.dto.MenuRequestDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InputView {
+    private final static String MENU_REGEX = "\\[(.*+),(\\d+),(\\d+)\\]";
     public int inputVendingMachineHoldingAmount() {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         return readInt();
     }
 
-    private int readInt() {
+    public int readInt() {
         String line = Console.readLine();
         try {
             return Integer.parseInt(line.trim());
@@ -16,5 +23,37 @@ public class InputView {
         catch (NumberFormatException e) {
             throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
+    }
+
+    public List<MenuRequestDto> inputMenus() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+        final String input = Console.readLine();
+        return parseMenus(input);
+    }
+
+    private List<MenuRequestDto> parseMenus(String input) {
+        List<MenuRequestDto> menuList = new ArrayList<>();
+
+        String[] splitMenu = input.split(";");
+        for (String menuInput : splitMenu) {
+            menuList.add(parseMenu(menuInput));
+        }
+
+        return menuList;
+    }
+
+    private MenuRequestDto parseMenu(String menuInput) {
+        Pattern pattern = Pattern.compile(MENU_REGEX);
+        Matcher matcher = pattern.matcher(menuInput);
+
+        if(!matcher.matches()) {
+            throw new IllegalArgumentException("올바른 입력 형식이 아닙니다.");
+        }
+
+        String name = matcher.group(1);
+        int price = Integer.parseInt(matcher.group(2));
+        int stockQuantity = Integer.parseInt(matcher.group(3));
+
+        return new MenuRequestDto(name, price, stockQuantity);
     }
 }
