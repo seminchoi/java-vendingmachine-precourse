281e65408d814cef89bca36cfc925b91ef797ce0
seminchoi <choicco89@gmail.com>
2023-12-05 15:38:46 +0900

    feat(domain.MenuBoard) : 메뉴판 생성자 및 검증 기능 구현

---
+++ src/main/java/vendingmachine/domain/MenuBoard.java
@@ -0,0 +1,22 @@
+package vendingmachine.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MenuBoard {
+    private final Map<String, Menu> menus = new HashMap<>();
+
+    public MenuBoard(final List<Menu> menus) {
+        for (final Menu menu : menus) {
+            addMenu(menu);
+        }
+    }
+
+    private void addMenu(final Menu menu) {
+        if(menus.containsKey(menu.getName())) {
+            throw new IllegalArgumentException("메뉴 이름은 중복될 수 없습니다.");
+        }
+        menus.put(menu.getName(), menu);
+    }
+}
