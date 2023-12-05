b88ebac968638db5755d883f6fd9c9b7f4e251df
seminchoi <choicco89@gmail.com>
2023-12-05 16:53:52 +0900

    feat(controller.Controller) : 자판기 보유 코인을 초기화하는 기능 구현

--- src/main/java/vendingmachine/controller/Controller.java
+++ src/main/java/vendingmachine/controller/Controller.java
@@ -1,5 +1,9 @@
 package vendingmachine.controller;
 
+import vendingmachine.domain.Coins;
+import vendingmachine.domain.Money;
+import vendingmachine.domain.RandomCoinGenerator;
+import vendingmachine.dto.CoinsResponseDto;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
 
@@ -10,5 +14,25 @@ public class Controller {
     public Controller(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void run() {
+        Coins havingCoins = initVendingMachineCoins();
+    }
+
+    private Coins initVendingMachineCoins() {
+        try {
+            final int amount = inputView.inputVendingMachineHoldingAmount();
+            final Money money = new Money(amount);
+            RandomCoinGenerator randomCoinGenerator = new RandomCoinGenerator();
+
+            Coins coins = randomCoinGenerator.generateCoins(money);
+            outputView.printVendingMachineCoins(CoinsResponseDto.of(coins));
+
+            return coins;
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e);
+            return initVendingMachineCoins();
+        }
     }
 }
