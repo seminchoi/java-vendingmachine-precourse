3f3935365f170ee0054921365bb9c1d93a642aa4
seminchoi <choicco89@gmail.com>
2023-12-05 14:59:26 +0900

    feat(domain.Coin) : 코인 금액 목록을 반환하는 기능과 금액으로 코인을 찾는 기능 구현

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -1,5 +1,13 @@
 package vendingmachine.domain;
 
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -12,5 +20,24 @@ public enum Coin {
         this.amount = amount;
     }
 
-    // 추가 기능 구현
+    public int getAmount() {
+        return amount;
+    }
+
+    private static final Map<Integer, Coin> amountCoin = Stream.of(values())
+            .collect(Collectors.toUnmodifiableMap(Coin::getAmount, Function.identity()));
+
+    public static List<Integer> getAvailableCoinAmounts() {
+        final List<Integer> coinAmounts = new LinkedList<>();
+
+        for (final Coin coin : values()) {
+            coinAmounts.add(coin.amount);
+        }
+        coinAmounts.sort(Comparator.reverseOrder());
+        return coinAmounts;
+    }
+
+    public static Coin findByAmount(final int amount) {
+        return amountCoin.get(amount);
+    }
 }
