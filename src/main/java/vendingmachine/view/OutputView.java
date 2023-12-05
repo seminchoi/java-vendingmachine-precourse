a0046ada934ce8a9d8ae5040c00181b69e8ad31a
seminchoi <choicco89@gmail.com>
2023-12-05 16:53:18 +0900

    feat(view.OutputView) : 자판기 보유 동전 출력 및 예외 출력 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -3,9 +3,14 @@ package vendingmachine.view;
 import vendingmachine.dto.CoinsResponseDto;
 
 public class OutputView {
+    private static final String ERROR_FORMAT = "[ERROR] %s\n";
     public void printVendingMachineCoins(CoinsResponseDto coins) {
         System.out.println("\n");
         System.out.println("자판기가 보유한 동전");
         System.out.println(coins);
+    }
+
+    public void printError(Exception e) {
+        System.out.printf(ERROR_FORMAT, e.getMessage());
     }
 }
