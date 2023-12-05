b42f77caf5fdd24a4b6325069ef0b86f219056f8
seminchoi <choicco89@gmail.com>
2023-12-05 14:18:54 +0900

    feat(domain.Money) : 금액 클래스 생성자 및 검증 기능 구현

---
+++ src/main/java/vendingmachine/domain/Money.java
@@ -0,0 +1,39 @@
+package vendingmachine.domain;
+
+public class Money {
+    private static final int MIN_AMOUNT_UNIT = 10;
+    private static final int MIN_AMOUNT = 100;
+    private static final int MAX_AMOUNT = 50_000;
+
+    private final int amount;
+
+    public Money(final int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
+
+    private void validate(final int amount) {
+        validateAmountUnit(amount);
+        validateAmountRange(amount);
+    }
+
+    private void validateAmountUnit(final int amount) {
+        if (isInvalidAmountUnit(amount)) {
+            throw new IllegalArgumentException("10원 단위로 입력해주세요.");
+        }
+    }
+
+    private boolean isInvalidAmountUnit(final int amount) {
+        return amount % MIN_AMOUNT_UNIT != 0;
+    }
+
+    private void validateAmountRange(final int amount) {
+        if (isInvalidAmountRange(amount)) {
+            throw new IllegalArgumentException("100원 이상 5만원 이하의 금액만 입력 가능합니다.");
+        }
+    }
+
+    private boolean isInvalidAmountRange(final int amount) {
+        return amount < MIN_AMOUNT || amount > MAX_AMOUNT;
+    }
+}
