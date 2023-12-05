b2637d5ee7b7237e429400b362239a642332b0b6
seminchoi <choicco89@gmail.com>
2023-12-05 17:48:10 +0900

    refactor(domain.coins) : 사용하지 않는 메소드 제거

--- src/main/java/vendingmachine/domain/Coins.java
+++ src/main/java/vendingmachine/domain/Coins.java
@@ -6,19 +6,6 @@ import java.util.Map;
 public class Coins {
     private final Map<Coin, Integer> coins = new EnumMap<>(Coin.class);
 
-    public int calculateTotalAmount() {
-        int totalAmount = 0;
-        for (final Coin coin : coins.keySet()) {
-            totalAmount += calculateCoinAmount(coin);
-        }
-
-        return totalAmount;
-    }
-
-    private int calculateCoinAmount(final Coin coin) {
-        return coin.getAmount() * coins.get(coin);
-    }
-
     public void addCoin(final Coin coin) {
         final int coinCount = coins.getOrDefault(coin, 0);
         coins.put(coin, coinCount + 1);
