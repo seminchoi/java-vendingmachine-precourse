c8cee87b8574e9dbc47132ebe641ecf65fa6de41
seminchoi <choicco89@gmail.com>
2023-12-05 15:06:17 +0900

    feat(domain.Coins) : 동전의 총 금액을 반환하는 기능 구현

--- src/main/java/vendingmachine/domain/Coins.java
+++ src/main/java/vendingmachine/domain/Coins.java
@@ -6,8 +6,21 @@ import java.util.Map;
 public class Coins {
     private final Map<Coin, Integer> coins = new EnumMap<>(Coin.class);
 
+    public int calculateTotalAmount() {
+        int totalAmount = 0;
+        for (final Coin coin : coins.keySet()) {
+            totalAmount += calculateCoinAmount(coin);
+        }
+
+        return totalAmount;
+    }
+
+    private int calculateCoinAmount(final Coin coin) {
+        return coin.getAmount() * coins.get(coin);
+    }
+
     public void addCoin(final Coin coin) {
-        int coinCount = coins.getOrDefault(coin, 0);
+        final int coinCount = coins.getOrDefault(coin, 0);
         coins.put(coin, coinCount + 1);
     }
 }
